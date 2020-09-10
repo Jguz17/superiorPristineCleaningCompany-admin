fff4feeb7a86a20ee60a06afc3c034d313ef3662
jguz17 <johnathanguzman17@gmail.com>
2020-09-09 19:02:19 -0500

    dashboard finished. need other pages done.

--- app-assets/js/bookings.js
+++ app-assets/js/bookings.js
@@ -1,26 +1,107 @@
-// document.querySelector('#booking-list').innerHTML += `<tr>
-// <td>Hard Coded</td>
-// <td>ABC Fintech LTD.</td>
-// <td>Jan 1,2019</td>
-// <td><span class="badge pink lighten-5 pink-text text-accent-2">Close</span></td>
-// <td>$ 1000.00</td>
-// <td class="center-align"><a href="#"><i class="material-icons pink-text">clear</i></a></td>
-// </tr>`
-async function getBookings() {
-    let response = await fetch('https://spcc-api.herokuapp.com/api/v1/bookings');
-    let data = await response.json();
+// ---- PENDING BOOKING TABLE ----
 
+async function loadBookings() {
+    let response = await fetch('https://spcc-api.herokuapp.com/api/v1/bookings');
+    let data = await response.json()
     data.map(booking => {
-        console.log(booking)
-        document.querySelector('#booking-list').innerHTML += `<tr>
+        booking.datetime =  new Date(booking.datetime).toLocaleString([], {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute:'2-digit'})
+        document.querySelector('#booking-list').innerHTML += `<tr data-id=${booking.id}>
             <td>${booking.name}</td>
             <td>${booking.email}</td>
             <td>${booking.phone}</td>
             <td>${booking.address}</td>
             <td>${booking.datetime}</td>
-            <td class="center-align"><a href="#"><i class="material-icons pink-text">clear</i></a></td>
+            <td class="center-align"><a><i style='color: #ff4081' onclick='removeBooking(this)' data-id=${booking.id} class="fas fa-times remove-booking"></i></a></td>
+            <td id='confirm' class="center-align"><a><i onclick='confirmBooking(this)' class="material-icons green-text confirm-booking">check</i></a></td>
             </tr>`
     })
 }
 
-getBookings()
+const removeBooking = (booking) => {
+    const bookingID = booking.parentElement.parentElement.parentElement.dataset.id
+    const row = booking.parentElement.parentElement.parentElement;
+    
+    fetch(`https://spcc-api.herokuapp.com/api/v1/bookings/${bookingID}`, { method: 'DELETE' })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+
+    row.remove()
+}
+
+const confirmBooking = (booking) => {
+    const bookingID = booking.parentElement.parentElement.parentElement.dataset.id
+    const row = booking.parentElement.parentElement.parentElement;
+
+    fetch(`https://spcc-api.herokuapp.com/api/v1/bookings/${bookingID}`)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        // const { address, datetime, email, message, name, phone, user_id } = data;
+        async function postBooking(url, obj) {
+            const response = await fetch(url, {
+                method: 'POST', 
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(obj)
+            })
+            return response.json(); 
+        }
+
+        postBooking('https://spcc-api.herokuapp.com/api/v1/accepted_bookings', data)
+        .then(data => {
+            console.log(data); 
+        });
+    })
+
+    fetch(`https://spcc-api.herokuapp.com/api/v1/bookings/${bookingID}`, { method: 'DELETE' })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+
+    row.remove()
+}
+
+// ---- CREATE BOOKING FORM ----
+
+document.querySelector('#create-booking-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const phone = document.querySelector('#phone').value;
+    const address = document.querySelector('#address').value;
+    const datetime = document.querySelector('#datetime').value;
+
+    const bookinObj = ({ user_id: 1, name, email, phone, address, datetime })
+
+    async function createBooking(url, obj) {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        })
+        return res.json()
+    }
+
+    createBooking('https://spcc-api.herokuapp.com/api/v1/bookings', bookinObj)
+    .then(data => {
+        console.log(data)
+        document.querySelector('#booking-list').innerHTML += `<tr data-id=${data.id}>
+        <td>${data.name}</td>
+        <td>${data.email}</td>
+        <td>${data.phone}</td>
+        <td>${data.address}</td>
+        <td>${data.datetime}</td>
+        <td class="center-align"><a><i onclick='removeBooking(this)' data-id=${data.id} class="fas fa-times remove-booking"></i></a></td>
+        <td id='confirm' class="center-align"><a><i style='color: #ff4081' onclick='confirmBooking(this)' class="material-icons green-text confirm-booking">check</i></a></td>
+        </tr>`
+    })
+
+    
+    
+
+})
+
+loadBookings()
